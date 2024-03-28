8342f25d327428aa24008b64750d25b6c191879a
mythicalBeast15x <ldonwulata@gmail.com>
2024-03-28 16:39:38 -0400

    Uploading H file

--- binaryFile.h
+++ binaryFile.h
@@ -1,5 +1,11 @@
 
 #ifndef WRITEBINARYFILE_BINARYFILE_H
 #define WRITEBINARYFILE_BINARYFILE_H
+#define MAX_NAME_LENGTH 50
+typedef struct{
+    char name[MAX_NAME_LENGTH];
+    char sender[MAX_NAME_LENGTH];
+    char receiver[MAX_NAME_LENGTH];
 
+} Message;
 #endif //WRITEBINARYFILE_BINARYFILE_H
